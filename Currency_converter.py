2718102f38e88ca1a6e472a0f0278ce683c15449
Ved <ved00684@gmail.com>
2024-12-13 20:05:11 +0530

    fixed the bugs

--- Currency_converter.py
+++ Currency_converter.py
@@ -3,7 +3,8 @@ import requests
 #? API and API Key are token from = # https://api.currencyapi.com
 your_currency="INR"
 converted_currency="USD"
-url=(f"https://api.currencyapi.com/v3/historical?apikey=cur_live_5YlcJZKgZJ4dI4ZUQ0kRdIMFydIyP8Krrudi4Zg9&currencies={converted_currency}&base_currency={your_currency}&date=2024-12-11")
+api_key=""
+url=(f"https://api.currencyapi.com/v3/historical?apikey={api_key}={converted_currency}&base_currency={your_currency}&date=2024-12-11")
 
 response=requests.get(url)
 data=response.json()
